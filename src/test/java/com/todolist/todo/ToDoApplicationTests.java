ac45374917eedcc48eaba4b29a17098759767a4c
Miguel <miguelgomezaguirre@hotmail.com>
2019-12-17 21:16:38 -0300

    first commit

---
+++ src/test/java/com/todolist/todo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
